897e76420dce0a32c2c62eb96c3e7eca262c9360
youtangai <youtanagai@gmail.com>
2018-01-24 01:33:33 +0900

    フォルダ名変更

---
+++ worker/config/main.go
@@ -0,0 +1,27 @@
+package config
+
+import "github.com/kelseyhightower/envconfig"
+
+type Configuration struct {
+	Port string `envconfig:"PORT" default:"62070"`
+}
+
+var (
+	config Configuration
+)
+
+const (
+	prefix = "TBS"
+)
+
+func init() {
+	envconfig.MustProcess(prefix, &config)
+}
+
+func reload() {
+	envconfig.Process(prefix, &config)
+}
+
+func Port() string {
+	return config.Port
+}
