76ff0aacabc0660a384a39f88645f55cf7bc2fa1
youtangai <youtanagai@gmail.com>
2018-01-24 01:02:19 +0900

    [update] 負荷指標を取得するプログラム完成

--- worker/main.go
+++ worker/main.go
@@ -3,17 +3,33 @@ package main
 import (
 	"log"
 	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	log.Println("Start Logging Load Average")
 	for {
-		output, err := exec.Command("uptime").Output()
+		cmdstr := "uptime | awk '$0 = $9' | sed -e 's/,//g'" // load average取得して 9番目のフィールド取得して カンマを削除
+		loadAverageByte, err := exec.Command("sh", "-c", cmdstr).Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(string(output))
+		loadAverageStr := strings.TrimRight(string(loadAverageByte), "\n")
+		loadAverageFloat, err := strconv.ParseFloat(loadAverageStr, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cpuNum := runtime.NumCPU()
+
+		loadIndicator := loadAverageFloat / float64(cpuNum)
+
+		log.Printf("Load Average =%f\n", loadAverageFloat)
+		log.Printf("CPU num =%d\n", cpuNum)
+		log.Printf("Load Average / CPU num =%f\n", loadIndicator)
 		time.Sleep(1 * time.Second)
 	}
 }
