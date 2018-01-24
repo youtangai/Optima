53aef377af43e074628bbbaab6faa60af88e6c32
youtangai <youtanagai@gmail.com>
2018-01-24 14:15:10 +0900

    worker ip 取得 ホスト名取得

--- worker/config/main.go
+++ worker/config/main.go
@@ -3,7 +3,8 @@ package config
 import "github.com/kelseyhightower/envconfig"
 
 type Configuration struct {
-	Port string `envconfig:"PORT" default:"62071"`
+	Port          string `envconfig:"PORT" default:"62071"`
+	ConductorHost string `envconfig:"CONDUCTOR_HOST" default:"conductor"`
 }
 
 var (
@@ -24,4 +25,8 @@ func reload() {
 
 func Port() string {
 	return config.Port
+}
+
+func ConductorHost() string {
+	return config.ConductorHost
 }
--- worker/main.go
+++ worker/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
@@ -11,25 +13,40 @@ import (
 
 func main() {
 	log.Println("Start Logging Load Average")
+	cpuNum := runtime.NumCPU()     //cpuの数を取得
+	hostname, err := os.Hostname() //ホストネームを取得
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addrs, err := net.InterfaceAddrs() //ネットワークインタフェースを複数取得
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := strings.Split(addrs[1].String(), "/")[0] //2つ目のインタフェースを取得して， /で分割して，ipだけ取得
+
+	log.Printf("network interface = %s\n", addr)
+
 	for {
 		cmdstr := "uptime | awk '$0 = $9' | sed -e 's/,//g'" // load average取得して 9番目のフィールド取得して カンマを削除
 		loadAverageByte, err := exec.Command("sh", "-c", cmdstr).Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		loadAverageStr := strings.TrimRight(string(loadAverageByte), "\n")
-		loadAverageFloat, err := strconv.ParseFloat(loadAverageStr, 32)
+		loadAverageStr := strings.TrimRight(string(loadAverageByte), "\n") //バイトをstringに変換して，改行を削除
+		loadAverageFloat, err := strconv.ParseFloat(loadAverageStr, 32)    //Floatに変換
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		cpuNum := runtime.NumCPU()
-
-		loadIndicator := loadAverageFloat / float64(cpuNum)
+		loadIndicator := loadAverageFloat / float64(cpuNum) //負荷指標を計算
 
 		log.Printf("Load Average =%f\n", loadAverageFloat)
 		log.Printf("CPU num =%d\n", cpuNum)
 		log.Printf("Load Average / CPU num =%f\n", loadIndicator)
+		log.Printf("Host name = %s\n", hostname)
+
 		time.Sleep(1 * time.Second)
 	}
 }
