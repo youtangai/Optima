641bbbb0d6e17f916ed72e7fd33f76be7f573d53
youtangai <youtanagai@gmail.com>
2018-01-24 14:19:32 +0900

    [fix] 関数としてまとめた

--- worker/main.go
+++ worker/main.go
@@ -19,34 +19,42 @@ func main() {
 		log.Fatal(err)
 	}
 
-	addrs, err := net.InterfaceAddrs() //ネットワークインタフェースを複数取得
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	addr := strings.Split(addrs[1].String(), "/")[0] //2つ目のインタフェースを取得して， /で分割して，ipだけ取得
-
+	addr := getNetIP()
 	log.Printf("network interface = %s\n", addr)
 
 	for {
-		cmdstr := "uptime | awk '$0 = $9' | sed -e 's/,//g'" // load average取得して 9番目のフィールド取得して カンマを削除
-		loadAverageByte, err := exec.Command("sh", "-c", cmdstr).Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-		loadAverageStr := strings.TrimRight(string(loadAverageByte), "\n") //バイトをstringに変換して，改行を削除
-		loadAverageFloat, err := strconv.ParseFloat(loadAverageStr, 32)    //Floatに変換
-		if err != nil {
-			log.Fatal(err)
-		}
+		loadAverage := getLoadAverage()
+		loadIndicator := loadAverage / float64(cpuNum) //負荷指標を計算
 
-		loadIndicator := loadAverageFloat / float64(cpuNum) //負荷指標を計算
-
-		log.Printf("Load Average =%f\n", loadAverageFloat)
+		log.Printf("Load Average =%f\n", loadAverage)
 		log.Printf("CPU num =%d\n", cpuNum)
 		log.Printf("Load Average / CPU num =%f\n", loadIndicator)
 		log.Printf("Host name = %s\n", hostname)
 
 		time.Sleep(1 * time.Second)
 	}
+}
+
+func getLoadAverage() float64 { //ロードアベレージを取得
+	cmdstr := "uptime | awk '$0 = $9' | sed -e 's/,//g'" // load average取得して 9番目のフィールド取得して カンマを削除
+	loadAverageByte, err := exec.Command("sh", "-c", cmdstr).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	loadAverageStr := strings.TrimRight(string(loadAverageByte), "\n") //バイトをstringに変換して，改行を削除
+	loadAverageFloat, err := strconv.ParseFloat(loadAverageStr, 32)    //Floatに変換
+	if err != nil {
+		log.Fatal(err)
+	}
+	return loadAverageFloat
+}
+
+func getNetIP() string { //IPを取得
+	addrs, err := net.InterfaceAddrs() //ネットワークインタフェースを複数取得
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := strings.Split(addrs[1].String(), "/")[0] //2つ目のインタフェースを取得して， /で分割して，ipだけ取得
+	return addr
 }
