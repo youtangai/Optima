b363eae2c31b6ff6db3be460e1273a291ee146a0
youtangai <youtanagai@gmail.com>
2018-01-25 16:25:46 +0900

    checkpointer testを修正

--- checkpointer/controller/main_test.go
+++ checkpointer/controller/main_test.go
@@ -3,7 +3,7 @@ package controller
 import "testing"
 
 func TestCheckpoint(t *testing.T) {
-	containerID := "thisistest"
+	containerID := "0825008df297"
 	result, err := checkpoint(containerID)
 	if err != nil {
 		t.Fatalf("failed with err = %v", err)
@@ -13,7 +13,7 @@ func TestCheckpoint(t *testing.T) {
 
 func TestScpCheckpointDir(t *testing.T) {
 	targetIP := "192.168.64.12"
-	sourceDir := "agagagaga"
+	sourceDir := "/tmp/0825008df2970c7cd25c5a7dafca2b0bfe8dabbc5a80ba0cb299ceea1083de79/chk"
 	err := scpCheckpointDir(targetIP, sourceDir)
 	if err != nil {
 		t.Fatalf("failed with err = %v", err)
