3a77479419ea4fd72234cac76c5818b384e32b2e
youtangai <youtanagai@gmail.com>
2018-01-25 15:11:20 +0900

    コメントを付与

--- checkpointer/controller/main.go
+++ checkpointer/controller/main.go
@@ -8,14 +8,23 @@ import (
 	"github.com/youtangai/Optima/checkpointer/model"
 )
 
+//CehckpointContainerController is チェックエンドポイントの処理
 func CehckpointContainerController(c *gin.Context) {
 	json := new(model.CheckpointContainerInfoJSON)
 	c.ShouldBindJSON(json)
 	containerID := json.ContainerID
 	targetIP := json.TargetIP
 
-	sourceDirPath := checkpoint(containerID)
-	err := scpCheckpointDir(targetIP, sourceDirPath)
+	sourceDirPath, err := checkpoint(containerID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	err = scpCheckpointDir(targetIP, sourceDirPath)
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -26,14 +35,17 @@ func CehckpointContainerController(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "checkpoint & scp done",
 	})
+	return
 }
 
-func checkpoint(containerID string) string {
+//コンテナをチェックポイントする関数
+func checkpoint(containerID string) (string, error) {
 	log.Printf("container id=%s\n", containerID)
 	chkDirPath := "/tmp/oiah3th4ihaoihgoiehoac/checkpoints/chk1"
-	return chkDirPath
+	return chkDirPath, nil
 }
 
+//sourceDirをtargetIPに送信する関数
 func scpCheckpointDir(targetIP, sourceDir string) error {
 	log.Printf("targetIP =%s\n", targetIP)
 	log.Printf("sourceDir =%s\n", sourceDir)
