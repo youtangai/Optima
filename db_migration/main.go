f49512b95e7d98e542e37e3d295c4cda904267e9
youtangai <youtanagai@gmail.com>
2018-01-26 04:03:48 +0900

    [db_migration] containerテーブルのmodelがただしいかどうか調べる

--- db_migration/main.go
+++ db_migration/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/youtangai/Optima/conductor/db"
 	"github.com/youtangai/Optima/conductor/model"
 )
 
 func main() {
 	db := db.GetDataBase()
+	db.LogMode(true)
 	if db.HasTable(&model.LoadIndicator{}) {
-		db.DropTable(&model.LoadIndicator{})
+		log.Println("load indicator is exist")
 	}
 	if db.HasTable(&model.Host{}) {
-		db.DropTable(&model.Host{})
+		log.Println("hosts is exist")
 	}
-	db.AutoMigrate(&model.Host{}, &model.LoadIndicator{})
+	//db.AutoMigrate(&model.Host{}, &model.LoadIndicator{})
+
+	db.SingularTable(true)
+	if db.HasTable(&model.Container{}) {
+		log.Println("find container table")
+	} else {
+		log.Println("could not find container table")
+	}
 }
