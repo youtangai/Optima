be1ae3adb3f054ff6fdfe80d94c087179dd9cd79
youtangai <youtanagai@gmail.com>
2018-01-25 14:28:39 +0900

    [checkpointer] main.goつくった

---
+++ checkpointer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	//PORT is ポート番号
+	PORT = "62072"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":" + PORT)
+}
