5a6749d1697cb46917b34b93b1b07398d582c390
youtangai <youtanagai@gmail.com>
2018-01-25 23:17:44 +0900

    [checkpointer] テスト修正

--- checkpointer/controller/main_test.go
+++ checkpointer/controller/main_test.go
@@ -10,19 +10,19 @@ const ID = "9277231bbe1634591433a66b4c907b32899a3b658641326494b83309bd784f2b"
 
 func TestCheckpoint(t *testing.T) {
 	containerID := ID
-	result, err := checkpoint(containerID)
+	err := checkpoint(containerID)
 	if err != nil {
 		t.Fatalf("failed with err = %v", err)
 	}
-	t.Logf("result = %s", result)
 }
 
 func TestScpCheckpointDir(t *testing.T) {
 	config.SetSecretKeyPath("/var/optima/optima_key")
 	config.SetControllerIP("192.168.64.12")
-	sourceDir := "/var/lib/docker/containers/" + ID + "/checkpoints/chk"
-	err := scpCheckpointDir(sourceDir)
+	containerID := ID
+	chkdir, err := scpCheckpointDir(containerID)
 	if err != nil {
 		t.Fatalf("failed with err = %v", err)
 	}
+	t.Logf("chkdir = %s", chkdir)
 }
