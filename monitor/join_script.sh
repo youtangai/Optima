64db79d7579a0065ee212c65e3e8e354604f0bb9
youtangai <youtanagai@gmail.com>
2018-01-26 13:30:50 +0900

    [monitor] joinscript修正

--- monitor/join_script.sh
+++ monitor/join_script.sh
@@ -7,5 +7,5 @@ if [ ! -d ${DIR} ]; then
     ssh-keygen -t rsa -f optima_key
     curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/create_dir
     scp ./optima_key.pub root@$CONDUCTOR_IP:/var/optima/$HOSTNAME/
-    curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/join
 fi
+curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/join
