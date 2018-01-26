30095b8b6e67ba04c81787cd527fbd5f1db3d9e0
youtangai <youtanagai@gmail.com>
2018-01-26 13:26:04 +0900

    monitor join_script 完成

--- monitor/join_script.sh
+++ monitor/join_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DIR="/var/optima"
 if [ ! -d ${DIR} ]; then
-    export CONDUCTOR_IP="192.168.64.12"
     echo $CONDUCTOR_IP
-    sudo mkdir -p ${DIR}
-    sudo cd /var
-    sudo cd /optima
-    sudo ssh-keygen -t rsa -f optima_key
-    sudo scp ./optima_key.pub root@$CONDUCTOR_IP:/var/optima/
+    mkdir -p ${DIR}
+    cd ${DIR}
+    ssh-keygen -t rsa -f optima_key
+    curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/create_dir
+    scp ./optima_key.pub root@$CONDUCTOR_IP:/var/optima/$HOSTNAME/
+    curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/join
 fi
