1fdfdd43125de8e65930b2799956d4da457ace39
youtangai <youtanagai@gmail.com>
2018-01-26 13:41:26 +0900

    monitor join_script修正

--- monitor/join_script.sh
+++ monitor/join_script.sh
@@ -4,6 +4,7 @@ if [ ! -d ${DIR} ]; then
     echo $CONDUCTOR_IP
     mkdir -p ${DIR}
     cd ${DIR}
+    pwd
     ssh-keygen -t rsa -f optima_key
     curl -X POST -d '{"host_name":"'$HOSTNAME'"}' -H "Content-Type:application/json" $CONDUCTOR_IP:62070/create_dir
     scp ./optima_key.pub root@$CONDUCTOR_IP:/var/optima/$HOSTNAME/
