1fbe65758b12366e68fd39b4fb40534adda46a96
youtangai <youtanagai@gmail.com>
2018-01-27 14:15:11 +0900

    conductor leaveのロジックを修正

--- conductor/controller/leave.go
+++ conductor/controller/leave.go
@@ -14,73 +14,84 @@ func LeaveController(c *gin.Context) {
 	json := new(model.LeaveJson)
 	c.ShouldBindJSON(json)
 	hostName := json.HostName
+	log.Printf("leave: hostname = %s", hostName)
 	//受け取ったホスト名のコンテナを調べる
 	containers, err := db.GetContainersByHostName(hostName)
 	if err != nil {
-		log.Fatal(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-		return
+		log.Fatal(err)
 	}
 	//コンテナの数が0なら終わり
 	if len(*containers) == 0 {
-		c.JSON(http.StatusOK, gin.H{"message": "no container need checkpoint&restore"})
+		log.Println("leave:delete load_indicator")
+		err = db.DeleteLoadIndicator(hostName)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			log.Fatal(err)
+		}
+		log.Println("leave:delete load_indicator done")
+		c.JSON(http.StatusOK, gin.H{"message": "no container need checkpoint and restore"})
 		return
 	}
 	//コンテナを１つ選択
 	for _, container := range *containers {
+		log.Printf("leave:current container info = %+v", container)
 		//同イメージでコンテナ作成を依頼 このとき uuidを取得
 		uuid, err := createContainer(container.Image)
+		log.Printf("leave:created container's uuid = %s", uuid)
 		if err != nil { //コンテナの作成ができなかったら
 			log.Println("leave:cannot create container")
 			//チェックポイントする
 			chkDirPath, err := checkpointContainer(container.ContainerID, container.Host)
 			if err != nil {
-				log.Fatal(err)
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-				return
+				log.Fatal(err)
 			}
+			log.Printf("leave:checkpointdirpath = %s", chkDirPath)
 			//chkdirpathをDBへ登録
-			db.RegistCheckPointDir(chkDirPath, container.Image)
+			err = db.RegistCheckPointDir(chkDirPath, container.Image)
+			if err != nil {
+				log.Println("leave:cannot regist chkdir into db")
+			}
 			//コンテナを削除する
 			err = deleteContainer(container.UUID)
 			if err != nil {
-				log.Fatal(err)
+				log.Println("cannot delete container")
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-				return
+				log.Fatal(err)
 			}
 			log.Println("leave:checkpoint container " + container.ContainerID)
+		} else {
+			log.Println("leave:created restore container")
+			//レストアするコンテナを取得
+			targetContainer, err := db.GetContainerByUUID(uuid)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+				log.Fatal(err)
+			}
+			log.Printf("leave:restore target container info = %+v", targetContainer)
+			//チェックポイントする
+			chkdirpath, err := checkpointContainer(container.ContainerID, container.Host)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+				log.Fatal(err)
+			}
+			log.Printf("leave:chkdirpaht = %s", chkdirpath)
+			//レストアする
+			err = restoreContainer(targetContainer.ContainerID, chkdirpath, targetContainer.Host)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+				log.Fatal(err)
+			}
+			log.Println("leave:restored container " + container.ContainerID)
 		}
-		log.Println("leave:created restore container")
-		//レストアするコンテナを取得
-		targetContainer, err := db.GetContainerByUUID(uuid)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-			return
-		}
-		//チェックポイントする
-		chkdirpath, err := checkpointContainer(container.ContainerID, container.Host)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-			return
-		}
-		//レストアする
-		err = restoreContainer(targetContainer.ContainerID, chkdirpath, targetContainer.Host)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-			return
-		}
-		log.Println("leave:restored container " + container.ContainerID)
 	}
 	//load_indicator削除
 	log.Println("leave:delete load_indicator")
 	err = db.DeleteLoadIndicator(hostName)
 	if err != nil {
-		log.Fatal(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-		return
+		log.Fatal(err)
 	}
 	log.Println("leave:delete load_indicator done")
 	c.JSON(http.StatusOK, gin.H{"message": "leave_suceed"})
